d9dae7037d91d9739b2f8984b7f4da2f713f93ff
Patricianduku <ndukupatricia136@gmail.com>
2025-07-20 22:02:35 +0300

    server.js changes

--- server/server.js
+++ server/server.js
@@ -31,11 +31,7 @@ app.use('/api/chat', chatRoutes);
 app.use('/api/newsletter', newsletterRoutes);
 app.use(errorHandler);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI);
 
 setupSocket(server);
 
